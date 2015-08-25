333f7b8a45adca8d5639a4d9bdcc9fe2ce6a8113
Brian Walters <brianw@pixotech.com>
2015-08-25 13:43:10 -0500

    A sample Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function( grunt ) {
+
+    //require('load-grunt-tasks')(grunt);
+
+    var taskConfig = {
+
+    };
+
+    //grunt.initConfig( taskConfig );
+
+    //grunt.registerTask( 'default', 'The default task', []);
+
+};
